aa92c0a7b2e6a1102043b8147c2b347eda7281e1
jubairahmed22 <jubairahmed060@gmail.com>
2023-03-29 22:41:28 +0600

    skill addded

--- src/Pages/Courses/CourseDetails.js
+++ src/Pages/Courses/CourseDetails.js
@@ -198,16 +198,69 @@ const CourseDetails = () => {
                     {
                         allCourses.map(skill =>
                             <div className='grid lg:grid-cols-3 gap-5 p-10'>
-                                <div className='p-2 text-lg font-bold rounded-full capsul transform transition duration-500 hover:scale-110 text-center tracking-wide items-center col-span-2'>{skill.skill1}</div>
-                                <div className='p-2 text-lg font-bold rounded-full capsul text-center transform transition duration-500 hover:scale-110 '>{skill.skill2}</div>
-                                <div className='p-2 text-lg font-bold rounded-full capsul text-center transform transition duration-500 hover:scale-110 '>{skill.skill3}</div>
-                                <div className='p-2 text-lg font-bold rounded-full capsul text-center col-span-2 tracking-wide transform transition duration-500 hover:scale-110 '>{skill.skill4}</div>
-                                <div className='p-2 text-lg font-bold rounded-full capsul text-center transform transition duration-500 hover:scale-110 '>{skill.skill5}</div>
-
-                                <div className='p-2 text-lg font-bold rounded-full capsul text-center transform transition duration-500 hover:scale-110 '>{skill.skill6}</div>
-                                <div className='p-2 text-lg font-bold rounded-full capsul text-center transform transition duration-500 hover:scale-110 '>{skill.skill7}</div>
-                                <div className='p-2 text-lg font-bold rounded-full capsul text-center transform transition duration-500 hover:scale-110 '>{skill.skill8}</div>
-                                <div className='p-2 text-lg font-bold rounded-full capsul text-center col-span-2 transform transition duration-500 hover:scale-110 '>{skill.skill9}</div>
+                                <a href="#_" class="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-orange-500 rounded-xl group">
+                                    <span class="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-orange-700 rounded group-hover:-mr-4 group-hover:-mt-4">
+                                        <span class="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
+                                    </span>
+                                    <span class="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full translate-y-full bg-orange-600 rounded-2xl group-hover:mb-12 group-hover:translate-x-0"></span>
+                                    <span class="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-white text-xl">{skill.skill1}</span>
+                                </a>
+                                <a href="#_" class="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-orange-500 rounded-xl group">
+                                    <span class="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-orange-700 rounded group-hover:-mr-4 group-hover:-mt-4">
+                                        <span class="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
+                                    </span>
+                                    <span class="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full translate-y-full bg-orange-600 rounded-2xl group-hover:mb-12 group-hover:translate-x-0"></span>
+                                    <span class="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-white text-xl">{skill.skill2}</span>
+                                </a>
+                                <a href="#_" class="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-orange-500 rounded-xl group">
+                                    <span class="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-orange-700 rounded group-hover:-mr-4 group-hover:-mt-4">
+                                        <span class="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
+                                    </span>
+                                    <span class="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full translate-y-full bg-orange-600 rounded-2xl group-hover:mb-12 group-hover:translate-x-0"></span>
+                                    <span class="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-white text-xl">{skill.skill3}</span>
+                                </a>
+                                <a href="#_" class="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-orange-500 rounded-xl group">
+                                    <span class="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-orange-700 rounded group-hover:-mr-4 group-hover:-mt-4">
+                                        <span class="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
+                                    </span>
+                                    <span class="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full translate-y-full bg-orange-600 rounded-2xl group-hover:mb-12 group-hover:translate-x-0"></span>
+                                    <span class="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-white text-xl">{skill.skill4}</span>
+                                </a>
+                                <a href="#_" class="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-orange-500 rounded-xl group">
+                                    <span class="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-orange-700 rounded group-hover:-mr-4 group-hover:-mt-4">
+                                        <span class="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
+                                    </span>
+                                    <span class="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full translate-y-full bg-orange-600 rounded-2xl group-hover:mb-12 group-hover:translate-x-0"></span>
+                                    <span class="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-white text-xl">{skill.skill5}</span>
+                                </a>
+                                <a href="#_" class="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-orange-500 rounded-xl group">
+                                    <span class="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-orange-700 rounded group-hover:-mr-4 group-hover:-mt-4">
+                                        <span class="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
+                                    </span>
+                                    <span class="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full translate-y-full bg-orange-600 rounded-2xl group-hover:mb-12 group-hover:translate-x-0"></span>
+                                    <span class="relative w-full text-left text-white transition-colors duration-200 ease-in-out text-xl group-hover:text-white">{skill.skill6}</span>
+                                </a>
+                                <a href="#_" class="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-orange-500 rounded-xl group">
+                                    <span class="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-orange-700 rounded group-hover:-mr-4 group-hover:-mt-4">
+                                        <span class="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
+                                    </span>
+                                    <span class="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full translate-y-full bg-orange-600 rounded-2xl group-hover:mb-12 group-hover:translate-x-0"></span>
+                                    <span class="relative w-full text-left text-white transition-colors duration-200 ease-in-out text-xl group-hover:text-white">{skill.skill7}</span>
+                                </a>
+                                <a href="#_" class="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-orange-500 rounded-xl group">
+                                    <span class="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-orange-700 rounded group-hover:-mr-4 group-hover:-mt-4">
+                                        <span class="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
+                                    </span>
+                                    <span class="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full translate-y-full bg-orange-600 rounded-2xl group-hover:mb-12 group-hover:translate-x-0"></span>
+                                    <span class="relative w-full text-left text-white transition-colors duration-200 ease-in-out text-xl group-hover:text-white">{skill.skill8}</span>
+                                </a>
+                                <a href="#_" class="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-orange-500 rounded-xl group">
+                                    <span class="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-orange-700 rounded group-hover:-mr-4 group-hover:-mt-4">
+                                        <span class="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
+                                    </span>
+                                    <span class="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full translate-y-full bg-orange-600 rounded-2xl group-hover:mb-12 group-hover:translate-x-0"></span>
+                                    <span class="relative w-full text-left text-xl text-white transition-colors duration-200 ease-in-out group-hover:text-white">{skill.skill9}</span>
+                                </a>
                             </div>
                         )
                     }
